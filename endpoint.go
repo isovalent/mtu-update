607a222d02c5b5db443283a6731cb18ed7ff662c
Joe Stringer <joe@covalent.io>
2018-06-25 17:49:47 -0700

    Fetch endpoints from Cilium
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- endpoint.go
+++ endpoint.go
@@ -15,8 +15,10 @@
 package main
 
 import (
-	"fmt"
 	"net"
+
+	"github.com/cilium/cilium/api/v1/models"
+	clientPkg "github.com/cilium/cilium/pkg/client"
 )
 
 // endpointInfo caches endpoint information from Cilium which will be useful
@@ -28,17 +30,76 @@ type endpointInfo struct {
 
 // managedIP returns true if the specified IP is a managed IP address.
 func (e *endpointInfo) managedIP(ip net.IP) bool {
+	_, ok := e.addrs[ip.String()]
+	return ok
+}
+
+// addIP attempts to insert the specified address into the endpoint info
+// structure. If the specified address is a valid IPv4 or IPv6 address, inserts
+// it and returns true. Otherwise, returns false.
+func (e *endpointInfo) addIP(addr string) bool {
+	ip := net.ParseIP(addr)
+	if ip != nil {
+		e.addrs[addr] = struct{}{}
+		return true
+	}
 	return false
 }
 
 // managedLink returns true if the link with the specified name is managed by
 // Cilium.
 func (e *endpointInfo) managedLink(name string) bool {
-	return false
+	_, ok := e.links[name]
+	return ok
+}
+
+// endpointInvalid returns true if the information we need from the provided
+// Endpoint object is missing.
+func endpointInvalid(ep *models.Endpoint) bool {
+	return ep == nil || ep.Status == nil || ep.Status.Networking == nil ||
+		len(ep.Status.Networking.Addressing) < 1 ||
+		ep.Status.Networking.InterfaceName == ""
+}
+
+// newEndpointInfoFromEndpoints creates a new endpointInfo structure from the
+// specified endpoint models. Logs errors if any endpoinst are invalid.
+func newEndpointInfoFromEndpoints(eps []*models.Endpoint) *endpointInfo {
+	result := &endpointInfo{
+		addrs: make(map[string]struct{}, len(eps)),
+		links: make(map[string]struct{}, len(eps)),
+	}
+	for _, ep := range eps {
+		if endpointInvalid(ep) {
+			log.Warnf("Found EP with invalid model: %+v", ep)
+			continue
+		}
+		netConfig := ep.Status.Networking
+		for _, addr := range netConfig.Addressing {
+			if !result.addIP(addr.IPV4) {
+				log.Warnf("Skipping invalid IP %s", addr.IPV4)
+			}
+			if !result.addIP(addr.IPV6) {
+				log.Warnf("Skipping invalid IP %s", addr.IPV6)
+			}
+		}
+		result.links[netConfig.InterfaceName] = struct{}{}
+	}
+
+	return result
 }
 
 // endpoints fetches information about endpoints from Cilium. Returns an error
 // if Cilium cannot be reached or listing the endpoints fails for any reason.
 func getEndpoints() (*endpointInfo, error) {
-	return nil, fmt.Errorf("not implemented")
+	client, err := clientPkg.NewClient("")
+	if err != nil {
+		return nil, err
+	}
+
+	eps, err := client.EndpointList()
+	if err != nil {
+		return nil, err
+	}
+
+	return newEndpointInfoFromEndpoints(eps), nil
 }
