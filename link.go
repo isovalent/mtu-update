35bb55a4a62f3691c124eb946163f60fe928e445
Joe Stringer <joe@covalent.io>
2018-06-25 18:49:27 -0700

    Find all links and configure them
    Signed-off-by: Joe Stringer <joe@covalent.io>

--- link.go
+++ link.go
@@ -16,17 +16,128 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/vishvananda/netlink"
 )
+
+const (
+	innerLinkType = "veth"
+)
+
+// linkInfo caches relevant information from the link / address probe for a
+// particular link on the system.
+type linkInfo struct {
+	netlink.Link
+	Addrs []netlink.Addr
+}
+
+func newLinkInfo(link netlink.Link, addrs []netlink.Addr) *linkInfo {
+	return &linkInfo{
+		Link:  link,
+		Addrs: addrs,
+	}
+}
 
 // scanLinks finds all links in the current namespace and returns them.
 func scanLinks() ([]netlink.Link, error) {
 	scopedLog.Debug("Fetching links")
 
-	return nil, fmt.Errorf("not implemented")
+	allLinks, err := netlink.LinkList()
+	if err != nil {
+		return nil, err
+	}
+
+	scopedLog.Debugf("Found %d links", len(allLinks))
+	for _, link := range allLinks {
+		attrs := link.Attrs()
+		scopedLog.Debugf("  %s", attrs.Name)
+		scopedLog.Debugf("    Type: %s", link.Type())
+		switch link.Type() {
+		case "device":
+			scopedLog.Debugf("    MTU: %d", attrs.MTU)
+		case "veth":
+			scopedLog.Debugf("    MTU: %d", attrs.MTU)
+		}
+	}
+
+	return allLinks, nil
+}
+
+// getPrimaryLink fetches the primary link in the current namespace - ie link
+// with the first ifindex (after loopback).
+func getPrimaryLink() (*linkInfo, error) {
+	links, err := scanLinks()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, link := range links {
+		if link.Type() == innerLinkType {
+			addrs, err := netlink.AddrList(link, netlink.FAMILY_V6)
+			if err != nil {
+				scopedLog.Infof("Failed to fetch address info for link %s", link.Attrs().Name)
+				continue
+			}
+			return newLinkInfo(link, addrs), nil
+		}
+	}
+
+	return nil, fmt.Errorf("failed to find primary link in %+v", links)
 }
 
 func updateHostLinks(allLinks []netlink.Link, deviceMTU int, epInfo *endpointInfo) {
-	log.Warn("Updating host links is not implemented")
+	log.Debug("Updating host namespace devices")
+	var (
+		skipped int
+		failed  int
+		updated int
+	)
+
+	// First, set all of the veths to allow reception of larger MTU.
+	ciliumLinks := make([]netlink.Link, 0, 4)
+	for _, link := range allLinks {
+		name := link.Attrs().Name
+		if link.Attrs().MTU == deviceMTU {
+			log.Debugf("Device %s has desired MTU", name)
+			skipped++
+			continue
+		}
+		if strings.HasPrefix(name, "cilium") {
+			// Don't count; just add to ciliumLinks
+			ciliumLinks = append(ciliumLinks, link)
+		} else if epInfo.managedLink(name) {
+			log.Debugf("Updating MTU for device %s", name)
+			if err := netlink.LinkSetMTU(link, deviceMTU); err == nil {
+				updated++
+			} else {
+				failed++
+				log.WithError(err).Warn(
+					"Failed to set link MTU for %s", name)
+			}
+		} else {
+			skipped++
+		}
+	}
+
+	// Next, set all of the cilium devices to allow transmit of larger MTU.
+	for _, link := range ciliumLinks {
+		name := link.Attrs().Name
+		if link.Attrs().MTU == deviceMTU {
+			log.Debugf("Device %s has desired MTU", name)
+			skipped++
+			continue
+		}
+		log.Debugf("Updating MTU for device %s", name)
+		if err := netlink.LinkSetMTU(link, deviceMTU); err == nil {
+			updated++
+		} else {
+			failed++
+			log.WithError(err).Warnf(
+				"Failed to set link MTU for %s", name)
+		}
+	}
+
+	log.Infof("Updated %d/%d local devices, %d skipped, %d failed",
+		updated, len(allLinks), skipped, failed)
 }
